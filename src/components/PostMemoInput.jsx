fb40185e59a31d651cda81f870d46e2804b32b0c
김가현 <rkgus0324@gmail.com>
2025-05-13 02:03:40 +0900

    ✨ [feat] : PostLocationInput, PostDateInput, PostMemoInput components 만들기 2025-ITShow-WePick#17

---
+++ src/components/PostMemoInput.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+const PostMemoInput = () => {
+  
+  return (
+    <div>
+      <label>메모 작성</label>
+      <textarea placeholder="짧은 메모를 작성해주세요" />
+    </div>
+  );
+};
+
+export default PostMemoInput;
