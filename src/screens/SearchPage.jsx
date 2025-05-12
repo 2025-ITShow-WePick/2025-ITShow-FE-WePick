6a8583dbd6e4afaf64a0e5b3e8497e09b78eeaf8
beebe0mg <w2321@e-mirim.hs.kr>
2025-05-12 10:40:44 +0900

    ✨ [feat] : logo components 만들기 #14

--- src/screens/SearchPage.jsx
+++ src/screens/SearchPage.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import styles from "../styles/SearchPage.module.css";
+import Logo from "../components/Logo";
 
 export default function SearchPage() {
   return (
     <>
-      <div>듀밥</div>
+      <Logo />
     </>
   );
 }
