f6ff83d86a862ebb3fc646a4783d00755d6eb795
김가현 <rkgus0324@gmail.com>
2025-05-13 02:08:21 +0900

    ✨ [feat] : App.jsx, main.jsx에 추가가2025-ITShow-WePick#20

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SplashPage from "./screens/SplashPage";
 import SearchPage from "./screens/SearchPage";
+import CreatePostPage from "./screens/CreatePostPage";
+// import PostTest from "./screens/PostTest";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<SplashPage />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/post" element={<CreatePostPage />} />
+        {/* <Route path="/posttest" element={<PostTest />} /> */}
       </Routes>
     </Router>
   );
