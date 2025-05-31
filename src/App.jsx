b65738bc3dc9520df24623bcfb66604799fc55d4
beebe0mg <w2321@e-mirim.hs.kr>
2025-05-31 22:35:07 +0900

    ✨ [feat] : 페이지 만들고 페이지 이동

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SplashPage from "./screens/SplashPage";
 import SearchPage from "./screens/SearchPage";
 import CreatePostPage from "./screens/CreatePostPage";
+import SearchPageDetail from "./screens/SearchPageDetail";
 // import PostTest from "./screens/PostTest";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SplashPage />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/searchdetail" element={<SearchPageDetail />} />
         <Route path="/post" element={<CreatePostPage />} />
         {/* <Route path="/posttest" element={<PostTest />} /> */}
       </Routes>
