ee495845ef164f34ebf6e2245a93480f03641fb9
beebe0mg <w2321@e-mirim.hs.kr>
2025-06-01 00:51:54 +0900

    ✨ [props] : 소소한 prop 전달.. 받기!

--- src/screens/SearchPageDetail.jsx
+++ src/screens/SearchPageDetail.jsx
@@ -2,11 +2,14 @@ import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../components/Logo";
 import styles from "../styles/SearchPageDetail.module.css";
+import LocationDate from "../components/LocationDate";
 
 export default function SearchPageDetail() {
   return (
     <>
       <Logo />
+      <LocationDate location="위치(지점)" />
+      <LocationDate location="날짜" />
     </>
   );
 }
