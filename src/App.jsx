4f2b2ebeabbe1f69ee6094a9cccfe2d9e39f1767
김가현 <rkgus0324@gmail.com>
2025-05-31 22:47:01 +0900

    fix: App.jsx 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SplashPage from "./screens/SplashPage";
 import SearchPage from "./screens/SearchPage";
-<<<<<<< Updated upstream
 import CreatePostPage from "./screens/CreatePostPage";
 import SearchPageDetail from "./screens/SearchPageDetail";
-=======
 import CreatePostPage2 from "./screens/CreatePostPage";
->>>>>>> Stashed changes
 // import PostTest from "./screens/PostTest";
 
 function App() {
@@ -16,13 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<SplashPage />} />
         <Route path="/search" element={<SearchPage />} />
-<<<<<<< Updated upstream
         <Route path="/searchdetail" element={<SearchPageDetail />} />
         <Route path="/post" element={<CreatePostPage />} />
         {/* <Route path="/posttest" element={<PostTest />} /> */}
-=======
         <Route path="/post" element={<CreatePostPage2 />} />
->>>>>>> Stashed changes
       </Routes>
     </Router>
   );
