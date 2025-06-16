84b1d8ccb79a32f73949a58afacae24d7bd4d049
beebe0mg <w2321@e-mirim.hs.kr>
2025-06-16 16:58:55 +0900

    ✨ [feat] : 배포한 서버 주소로 연결 변동

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      "/api": {
+        // /api로 시작하는 모든 요청
+        target: "http://52.78.100.102:3000",
+        changeOrigin: true,
+      },
       "/post": {
-        target: "http://localhost:3000",
+        target: "http://52.78.100.102:3000",
         changeOrigin: true,
       },
       "/user": {
-        target: "http://localhost:3000",
+        target: "http://52.78.100.102:3000",
+        changeOrigin: true,
+      },
+      "/upload": {
+        // 업로드 관련 요청
+        target: "http://52.78.100.102:3000",
         changeOrigin: true,
       },
     },
